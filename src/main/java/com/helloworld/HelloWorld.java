dcba62347f3378b5a7642611261b23d83ab9f98e
qwa-rwansouza <rwan.souza@qwasolucoes.com.br>
2024-11-28 09:09:39 -0300

    understanding basics how the session and transaction mechanism works, along with the auto-generation of sequences managed by Hibernate.

--- src/main/java/com/helloworld/HelloWorld.java
+++ src/main/java/com/helloworld/HelloWorld.java
@@ -1,17 +1,41 @@
 package com.helloworld;
 
+import java.util.List;
+import java.util.Map;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class HelloWorld {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        EntityManagerFactory entt = Persistence.createEntityManagerFactory("Hibernate-PU");
-        EntityManager enttManger = entt.createEntityManager();
+		EntityManagerFactory entt = Persistence.createEntityManagerFactory("Hibernate-PU");
+		EntityManager enttManger = entt.createEntityManager();
 
-        enttManger.close();
-        entt.close();
+		Message message = new Message("Hello World");
 
-    }
+		enttManger.getTransaction().begin();
+		enttManger.persist(message);
+		enttManger.getTransaction().commit();
+
+		List<Message> messages = enttManger.createQuery("from Message m order by m.text asc").getResultList();
+
+		System.out.println(messages.size() + " message(s) found: ");
+
+		for (Message loadedMsg  :messages) {
+			System.out.println(loadedMsg.getText());
+		}
+		
+		enttManger.getTransaction().begin();
+		
+		Message find = enttManger.find(Message.class, 1);
+		
+		find.setText("Greetings Earthling");
+		find.setNextMessage(new Message("Take me to your leader (please)"));
+		
+		enttManger.getTransaction().commit();
+		enttManger.close();
+		entt.close();
+	}
 }
